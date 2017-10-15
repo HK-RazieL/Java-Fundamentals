1606be8eea75bad8f726fdd98c00ba3c96f4672d
HK-RazieL <s.angelkov@abv.bg>
2017-10-15 07:08:44 +0300

    Stream API - Lab

--- Java Advanced/Functional Programming - Exercises/src/P01_ConsumerPrint.java
+++ Java Advanced/Functional Programming - Exercises/src/P01_ConsumerPrint.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.function.Consumer;
+
 public class P01_ConsumerPrint {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = reader.readLine().split(" ");
+
+        Consumer<String> consumer = x -> System.out.println(x);
+
+        for (int i = 0; i < input.length; i++) {
+            consumer.accept(input[i]);
+        }
     }
 }
